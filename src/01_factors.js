ccae1e2f283ab02947fe7257697f386c33cb2962
Marissa Reinke <reinke.marissa@gmail.com>
2018-02-11 09:52:36 -0700

    Solve factors and reduce

--- src/01_factors.js
+++ src/01_factors.js
@@ -1,6 +1,6 @@
 /*--FACTORS
 
-  Write a function that returns an array 
+  Write a function that returns an array
   containing all of the factors of a number.
 
   For example: factors(10) returns [1, 2, 5, 10]
@@ -9,7 +9,21 @@
 var factors;
 
 factors = function(num) {
-  return "summon here";
+  var factors = [];
+
+  for (var i = 0; i <= num; i++) {
+    if (num % i === 0) {
+      factors.push(i);
+    }
+  }
+
+  if (factors.length) {
+    return factors
+  } else {
+    return null
+  }
+
+  // factors.length != 0 ? factors : null;
 };
 
 /*--Mocha Testing--*/
