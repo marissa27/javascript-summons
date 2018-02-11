29abbaafa50aa101edb8b81301d155649505b0d3
Marissa Reinke <reinke.marissa@gmail.com>
2018-02-11 13:19:37 -0700

    Solve and make spec 4 harder

--- src/04_median.js
+++ src/04_median.js
@@ -1,4 +1,4 @@
-git remote/*--MEDIAN
+/*--MEDIAN
 
   Write a function that finds the median of a given array of integers.
   If the array has an odd number of integers, return the middle item
@@ -11,8 +11,22 @@ git remote/*--MEDIAN
 var median;
 
 median = function(arr) {
-  return "summon here";
+  const reducer = (acc, val) => acc + val;
+
+  if (!arr.length) {
+    return arr
+  } else if (arr.length % 2 === 0) {
+    return (arr.reduce(reducer) / arr.length).toFixed();
+  } else {
+    let sortedArr = arr.sort(numberSort);
+    let middle = (arr.length / 2).toFixed() - 1;
+    return sortedArr[middle]
+  }
 };
+
+function numberSort(a, b) {
+  return a - b;
+}
 
 /*--Mocha Testing--*/
 
